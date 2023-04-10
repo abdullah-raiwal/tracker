19fd42364de3c4e4e1fe2f9b373d5cade09c2faa
abdullah-raiwal <abdullahh.qaiser@gmail.com>
2023-04-10 22:58:30 +0500

    changed sqlite to postgres

--- tracker/settings.py
+++ tracker/settings.py
@@ -108,14 +108,23 @@ WSGI_APPLICATION = "tracker.wsgi.application"
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-        'OPTIONS': {
-            'timeout': 20,
-            'deterministic': False,
-        },
+    # "default": {
+    #     "ENGINE": "django.db.backends.sqlite3",
+    #     "NAME": BASE_DIR / "db.sqlite3",
+
+    # }
+
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'railway',
+        'USER': 'postgres',
+        'PASSWORD': 'FmOSDIEuBnzHMmmYd4a7',
+        'HOST': 'containers-us-west-3.railway.app',
+        'PORT': '8021',
     }
+
+
+
 }
 
 
